c72da2b4ea26c12aabad156de78ca51c439980a7
yyj <yuyinjian@mafengwo.com>
2019-06-03 20:33:15 +0800

    modify router config

--- src/pages/index/router.ts
+++ src/pages/index/router.ts
@@ -8,6 +8,11 @@ export default new Router({
   base: 'apimanage/',
   routes: [
     {
+      path: '/',
+      name: 'index',
+      component: () => import(/* webpackChunkName: "indexPage" */ './views/Dashboard.vue'),
+    },
+    {
       path: '/dashboard',
       name: 'dashboard',
       component: () => import(/* webpackChunkName: "about" */ './views/Dashboard.vue'),
@@ -16,6 +21,51 @@ export default new Router({
       path: '/myClient',
       name: 'myClient',
       component: () => import(/* webpackChunkName: "myClient" */ './views/MyClient.vue'),
+    },
+    {
+      path: '/clientApply',
+      name: 'clientApply',
+      component: () => import(/* webpackChunkName: "clientApply" */ './views/ClientApply.vue'),
+    },
+    {
+      path: '/serviceRegister',
+      name: 'serviceRegister',
+      component: () => import(/* webpackChunkName: "serviceRegister" */ './views/ServiceRegister.vue'),
+    },
+    {
+      path: '/serviceApply',
+      name: 'serviceApply',
+      component: () => import(/* webpackChunkName: "serviceApply" */ './views/ServiceApply.vue'),
+    },
+    {
+      path: '/myClass',
+      name: 'myClass',
+      component: () => import(/* webpackChunkName: "myClass" */ './views/MyClass.vue'),
+    },
+    {
+      path: '/createClass',
+      name: 'createClass',
+      component: () => import(/* webpackChunkName: "createClass" */ './views/CreateClass.vue'),
+    },
+    {
+      path: '/editSource',
+      name: 'editSource',
+      component: () => import(/* webpackChunkName: "editSource" */ './views/EditSource.vue'),
+    },
+    {
+      path: '/clientApplyAboutMe',
+      name: 'clientApplyAboutMe',
+      component: () => import(/* webpackChunkName: "clientApplyAboutMe" */ './views/ClientApplyAboutMe.vue'),
+    },
+    {
+      path: '/serviceList',
+      name: 'serviceList',
+      component: () => import(/* webpackChunkName: "serviceList" */ './views/ServiceList.vue'),
+    },
+    {
+      path: '/serviceDetail',
+      name: 'serviceDetail',
+      component: () => import(/* webpackChunkName: "serviceDetail" */ './views/ServiceDetail.vue'),
     },
   ],
 });
