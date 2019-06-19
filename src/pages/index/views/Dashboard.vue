02bc518763a5ed8051f82ad1a0f1ff97ee99551d
forijk <forijk@163.com>
2019-06-19 17:52:28 +0800

    modify dashboard

--- src/pages/index/views/Dashboard.vue
+++ src/pages/index/views/Dashboard.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
-    <h1>Dashboard page!!!</h1>
+    <h1>Vue-typescript-element-template</h1>
+    <p>
+      一款轻量级、简单易用、结构清晰的 Vue CLI3 + TypeScript + ElementUI
+      开发的企业级后台模板。
+    </p>
+    <a
+      href="https://github.com/forijk/vue-typescript-element-template"
+      target="_blank"
+      >Github 源码地址</a
+    >
   </div>
 </template>
 
